a1285b80e536cdd8231dd782a8a06afe44f01091
SimiVoid <simivoid@gmail.com>
2022-06-06 21:39:48 +0200

    Fix -O3 flag compilation error

--- include/enigma_decoding.h
+++ include/enigma_decoding.h
@@ -1,10 +1,6 @@
 #ifndef _ENIGMA_DECODING_H_
 #define _ENIGMA_DECODING_H_
 
-inline char* enigma_decoding(char* input) {
-    extern char* enigma_encoding(char* input);
-    
-    return enigma_encoding(input);
-}
+char* enigma_decoding(char* input);
 
 #endif
---
+++ src/enigma_decoding.c
@@ -0,0 +1,6 @@
+#include <enigma_decoding.h>
+#include <enigma_encoding.h>
+
+char* enigma_decoding(char* input) {
+    return enigma_encoding(input);
+}
